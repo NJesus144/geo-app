8ad9a5ddbb31194565676fb4c8adb21ecee51ec4
NJesus144 <nalbert-edm@outlook.com>
2025-02-25 16:55:19 -0300

    add environment configuration, update .gitignore, and modify Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  transpilePackages: ['react-leaflet'],
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  }
 }
 
 export default nextConfig
